28d1da226f8c7613f81fa0aed01541f6586abf42
Kazuma SATO <ranceworks@gmail.com>
2015-12-07 06:16:30 +0900

    #10 upgrade this project's version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject kalar-plugins "0.1.0"
+(defproject kalar-plugins "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.raynes/fs "1.4.6"]
-                 [kalar-core "0.1.0-SNAPSHOT"]
+                 [kalar-core "0.1.1-SNAPSHOT"]
                  [markdown-clj "0.9.82"]
                  [enlive "1.1.6"]
                  ])
