e6d032d54da4db2ca5e0c687dc5bf0b686951f49
Kazuma SATO <ranceworks@gmail.com>
2015-12-07 06:12:54 +0900

    #10 upgrade this project's version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kalar-plugins "0.1.0-SNAPSHOT"
+(defproject kalar-plugins "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
