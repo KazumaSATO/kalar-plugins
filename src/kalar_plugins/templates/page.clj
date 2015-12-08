5fe135569677fb417facc2e44e85eb69c2c9c1c8
Kazuma SATO <ranceworks@gmail.com>
2015-12-09 07:23:33 +0900

    #7 add next and prev urls

--- src/kalar_plugins/templates/page.clj
+++ src/kalar_plugins/templates/page.clj
@@ -7,15 +7,34 @@
             [kalar-core.file :as kfile]
             [hiccup.page :as hpage]))
 
+(defn- get-dst-path [^String src-path]
+  (kfile/get-dst (str/replace src-path #"\..*$" ".html")))
 
 (defn- compile-md [file]
   (let [file-path (.getAbsolutePath file)
         md (hp/load-markdown file-path)
-        dst (kfile/get-dst (str/replace file-path #"\..*$" ".html"))
+        dst (get-dst-path file-path)
         fnc (-> md :template first)]
     (kfile/touch dst)
     (require (symbol (str/replace fnc  #"/.*"  "")))
     (spit dst ((var-get (resolve (symbol fnc))) md))))
+
+(defn- create-neighbor-url [urls]
+  (map (fn [p n] {:previous-page p :next-page n})
+       (cons nil (drop-last urls))
+       (concat (rest urls) '(nil))))
+
+(defn- compile-serial-mds [dir]
+  (let [files (.listFiles (io/file dir))
+        mds (map #(merge (hp/load-md-excerpt (.getAbsolutePath %))
+                         {:url (get-dst-path (.getAbsolutePath %))})
+                 files)
+        neighbor-urls (create-neighbor-url (map #(.getAbsolutePath (:url %)) mds))
+        mds-with-neighbors (map (fn [m n] (merge m n)) mds neighbor-urls)
+        ]
+    (comment (partition-all))
+    (println mds-with-neighbors)
+    ))
 
 (plugin/defkalar-plugin
   page
@@ -24,6 +43,8 @@
     [this]
     (dorun
       (for [file (.listFiles (io/file (:page-dir (config/read-config))))]
-        (compile-md file)))))
+        (compile-md file)))
+    (compile-serial-mds (:posts-dir (config/read-config)))
+    ))
 
 
