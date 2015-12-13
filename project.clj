02fca9bb068032073e47481a31c8e9a2b9431890
Kazuma SATO <ranceworks@gmail.com>
2015-12-13 16:28:45 +0900

    v0.1.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject kalar-plugins "0.1.1-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject kalar-plugins "0.1.2"
+  :description "plugins of kalar"
   :url "https://github.com/KazumaSATO/kalar-plugins"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.raynes/fs "1.4.6"]
-                 [kalar-core "0.1.1-SNAPSHOT"]
+                 [kalar-core "0.1.2"]
                  [markdown-clj "0.9.82"]
                  [enlive "1.1.6"]
                  ])
