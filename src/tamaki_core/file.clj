62c845e39c05254b57a1b03bc41332e7d2283fcd
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-01-24 20:54:56 +0900

    #28 add the function to clean the destination

--- src/tamaki_core/file.clj
+++ src/tamaki_core/file.clj
@@ -1,23 +1,32 @@
-(ns kalar-core.file
+(ns tamaki-core.file
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [kalar-core.config :as config]))
+            [tamaki-core.config :as config]))
 
-(defn find-resources-dir []
-  "Return the absolute path of resources"
-  (.getAbsolutePath (io/file "resources")))
+(comment
+  (defn find-resources-dir []
+    "Return the absolute path of resources"
+    (.getAbsolutePath (io/file "resources"))))
 
-(defn find-dest []
-  "Returns a build destination as string."
-  (let [f (io/file (find-resources-dir) (:dest (config/read-config)))]
-    (.getAbsolutePath f)))
+(comment
+  (defn find-dest []
+    "Returns a build destination as string."
+    (let [f (io/file (find-resources-dir) (:dest (config/read-config)))]
+      (.getAbsolutePath f))))
 
-(defn touch [maybe-unexist-file]
-  "Refactoring: rename to create-empty-file"
-  (.mkdirs (.getParentFile maybe-unexist-file))
-  (.createNewFile maybe-unexist-file))
+(defn create-empty-file [maybe-unexists-filename]
+  "Create an empty file to be written afterwards."
+  (let [file (io/file maybe-unexists-filename)]
+    (.mkdirs (.getParentFile file))
+    (.createNewFile file)))
+
+(defn clean-dest
+  "Make the destination empty."
+  ([] (clean-dest (-> (config/read-config) :dest io/file)))
+  ([dest] (doseq [file (.listFiles dest)] (.delete file))))
 
 
-(defn get-dst [file-path]
-  "Return java.io.File"
-  (io/file (find-dest) (str/replace file-path (re-pattern (str "^" (find-resources-dir) "/")) "")))
+(comment
+  (defn get-dst [file-path]
+    "Return java.io.File"
+    (io/file (find-dest) (str/replace file-path (re-pattern (str "^" (find-resources-dir) "/")) ""))))
