0bc60f4c17dc6253cba8a104ef58f962e53ccd39
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-01-24 20:57:52 +0900

    remove commented fuctions #28

--- src/tamaki_core/file.clj
+++ src/tamaki_core/file.clj
@@ -1,18 +1,7 @@
 (ns tamaki-core.file
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
             [tamaki-core.config :as config]))
 
-(comment
-  (defn find-resources-dir []
-    "Return the absolute path of resources"
-    (.getAbsolutePath (io/file "resources"))))
-
-(comment
-  (defn find-dest []
-    "Returns a build destination as string."
-    (let [f (io/file (find-resources-dir) (:dest (config/read-config)))]
-      (.getAbsolutePath f))))
 
 (defn create-empty-file [maybe-unexists-filename]
   "Create an empty file to be written afterwards."
@@ -24,9 +13,3 @@
   "Make the destination empty."
   ([] (clean-dest (-> (config/read-config) :dest io/file)))
   ([dest] (doseq [file (.listFiles dest)] (.delete file))))
-
-
-(comment
-  (defn get-dst [file-path]
-    "Return java.io.File"
-    (io/file (find-dest) (str/replace file-path (re-pattern (str "^" (find-resources-dir) "/")) ""))))
