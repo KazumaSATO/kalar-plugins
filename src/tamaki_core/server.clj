e6b71d679f72f4a94d59abc090da57aeae140f6d
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-01-24 21:19:41 +0900

    #28 comment

--- src/tamaki_core/server.clj
+++ src/tamaki_core/server.clj
@@ -1,32 +1,35 @@
 (ns kalar-core.server
-  (:require [kalar-core.config :refer [read-config]]
+  (:require [tamaki-core.config :refer [read-config]]
             [kalar-core.file.tracker :as tracker]
              [compojure.core :refer [GET defroutes]]
              [compojure.route :as route]
              [ring.util.response :refer [redirect]]
              ))
 
-(defn load-plugins []
-  (doseq [plugin (-> (read-config) :plugins)]
-    (require plugin)
-    ((-> (symbol (str plugin "/" 'load-plugin)) resolve var-get))))
+(comment
+  (defn load-plugins []
+    (doseq [plugin (-> (read-config) :plugins)]
+      (require plugin)
+      ((-> (symbol (str plugin "/" 'load-plugin)) resolve var-get)))))
 
-(defn init []
-  (load-plugins))
+(comment
+  (defn init []
+    (load-plugins)))
 
 (defroutes handler
   (GET ":prefix{.*}/" [prefix] (redirect (str prefix "/index.html")))
   (route/resources "/" {:root (:dest (read-config))})
   (route/not-found "Page not found"))
 
-(def ^{:private true} track (tracker/track "resources"))
+(comment (def ^{:private true} track (tracker/track "resources")))
 
-(defn- wrap-tracker [handler]
-  (fn [request]
-    (let [diff (track)]
-      (if (not (and (empty? (:removed diff)) (empty? (:created diff))))
-        (load-plugins))
-      (handler request))))
+(comment
+  (defn- wrap-tracker [handler]
+    (fn [request]
+      (let [diff (track)]
+        (if (not (and (empty? (:removed diff)) (empty? (:created diff))))
+          (load-plugins))
+        (handler request)))))
 
 
-(def app (wrap-tracker handler))
+(comment (def app (wrap-tracker handler)))
