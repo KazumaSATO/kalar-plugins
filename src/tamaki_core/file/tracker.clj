7b9e2ac5edb26e8d0ee0fd8335b80b732919ed44
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-01-24 21:13:41 +0900

    #28 comment

--- src/tamaki_core/file/tracker.clj
+++ src/tamaki_core/file/tracker.clj
@@ -6,38 +6,43 @@
 
 ; FIXME
 
-(defn- create-modtime-map [^String directory]
-  (letfn
-    [(create-modtime-map [efile]
-       {(keyword (.getAbsolutePath efile)) (.lastModified efile)})
-     (merge-map [efiles]
-       (reduce #(merge %1 (create-modtime-map %2)) {} efiles))
-     (get-child-map [dir]
-       (let [children (.listFiles dir)
-             thismap (merge-map children)
-             child-dirs (filter #(and (not (= (.getAbsolutePath (io/file (:dest (config/read-config))))
-                                              (.getAbsolutePath %)))
-                                      (.isDirectory %))  children)]
-         (reduce #(merge %1 %2) thismap
-                 (for [m child-dirs] (get-child-map m))))
-       )]
-    (get-child-map (io/file directory))))
+(comment
+  (defn- create-modtime-map [^String directory]
+    (letfn
+      [(create-modtime-map [efile]
+         {(keyword (.getAbsolutePath efile)) (.lastModified efile)})
+       (merge-map [efiles]
+         (reduce #(merge %1 (create-modtime-map %2)) {} efiles))
+       (get-child-map [dir]
+         (let [children (.listFiles dir)
+               thismap (merge-map children)
+               child-dirs (filter #(and (not (= (.getAbsolutePath (io/file (:dest (config/read-config))))
+                                                (.getAbsolutePath %)))
+                                        (.isDirectory %))  children)]
+           (reduce #(merge %1 %2) thismap
+                   (for [m child-dirs] (get-child-map m))))
+         )]
+      (get-child-map (io/file directory)))))
 
 
-(defn- find-removed [old current]
-  (keys (first (data/diff old current))))
-(defn- find-created [old current]
-  (keys (nth (data/diff old current) 1)))
+(comment
+  (defn- find-removed [old current]
+    (keys (first (data/diff old current)))))
 
-(defn track
-  ([root] (track root (create-modtime-map root)))
-  ([root timestamp-map]
-   (let [timemap (atom timestamp-map)]
-     (fn []
-       (let [then @timemap
-             now (create-modtime-map root)
-             diff {:removed (find-removed then now)
-                   :created (find-created then now)}]
-         (reset! timemap now)
-         diff)))))
+(comment
+  (defn- find-created [old current]
+    (keys (nth (data/diff old current) 1))))
+
+(comment
+  (defn track
+    ([root] (track root (create-modtime-map root)))
+    ([root timestamp-map]
+     (let [timemap (atom timestamp-map)]
+       (fn []
+         (let [then @timemap
+               now (create-modtime-map root)
+               diff {:removed (find-removed then now)
+                     :created (find-created then now)}]
+           (reset! timemap now)
+           diff))))))
 
