4aa55cada2fe3162bba94dd693036cd9ef273a5e
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-03-07 06:37:46 +0900

    improve a test #29

--- test/tamaki/post/post_test.clj
+++ test/tamaki/post/post_test.clj
@@ -1,8 +1,10 @@
 (ns tamaki.post.post-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
+            [me.raynes.fs :as rfs]
             [tamaki.post.post :as post]))
 
 (deftest loading-posts
   (testing "find the post files "
-    (is (some? (post/post-seq (io/file "dev-resources/tamaki/post"))))))
+    (let [filenames (map #(rfs/base-name %)  (post/post-seq (io/file "dev-resources/tamaki/post")))]
+      (is (= #{"post.md" "childpost.md"} (set filenames))))))
