ab162ab9b2db95e5deb2a51bab9e3630ccde5888
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-03-14 07:32:09 +0900

    #29 add another function to read a post md

--- src/tamaki/text/html.clj
+++ src/tamaki/text/html.clj
@@ -5,19 +5,20 @@
            (clojure.lang LazySeq PersistentArrayMap PersistentStructMap)))
 
 
-(defn- create-enlive-mdl [html-text]
-  (ehtml/html-resource (StringReader. html-text)))
+(comment
+  (defn- create-enlive-mdl [html-text]
+    (ehtml/html-resource (StringReader. html-text))))
 
 (defn extract-text [enlive-model]
   "extract plain text from an enlive model"
   (letfn [(extrct-txt [model acc]
-           (cond
-             (instance? LazySeq model) (reduce #(str %1 (extrct-txt %2 "")) "" model)
-             (map? model) (if (= :script (:tag model))
-                            acc
-                            (extrct-txt (:content model) acc))
-             (instance? String model) (str acc model)
-             (nil? model) acc
-             :else (log/error "unexpected argment: " (type model)  model)))]
+            (cond
+              (instance? LazySeq model) (reduce #(str %1 (extrct-txt %2 "")) "" model)
+              (map? model) (if (= :script (:tag model))
+                             acc
+                             (extrct-txt (:content model) acc))
+              (instance? String model) (str acc model)
+              (nil? model) acc
+              :else (log/error "unexpected argment: " (type model) model)))]
     (extrct-txt enlive-model "")))
 
