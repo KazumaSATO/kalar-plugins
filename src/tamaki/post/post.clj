f25750eb03069d8582ece1e36ef15988b9414b1a
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-03-15 07:48:14 +0900

    #29 import functions for post similarity

--- src/tamaki/post/post.clj
+++ src/tamaki/post/post.clj
@@ -1,11 +1,42 @@
 (ns tamaki.post.post
   (:require [tamaki-core.config :as config]
-            [me.raynes.fs :as rfs]
-            [clojure.java.io :as io]))
+            [me.raynes.fs :as fs]
+            [tamaki.template.page :as tpage]
+            [tamaki.text.html :as thtml]
+            [tamaki.post.post :as tpost]
+            [tamaki.text.similarity :as simi]
+            [net.cgrand.enlive-html :as ehtml]
+            [clojure.java.io :as io])
+  (:import (java.io StringReader)))
 
 
 (defn post-seq
   "return the post files"
   ([post-dir]
-   (filter #(rfs/file? %) (file-seq post-dir)))
+   (filter #(fs/file? %) (file-seq post-dir)))
   ([] (post-seq (io/file (:post-dir (config/read-config))))))
+
+(defn calc-post-similarity
+  ([] (calc-post-similarity (-> (config/read-config) :post-dir) (-> (config/read-config) :dest)))
+  ([post-dir dest-root]
+   (letfn [(path-body [post]
+             (let [path (.getPath post)
+                   md (tpage/read-postmd path dest-root)
+                   body (thtml/extract-text (ehtml/html-resource (StringReader. (:body md))))]
+               {:key path  :text body}))
+           ]
+     (let [key-texts (map #(path-body %) (tpost/post-seq (io/file post-dir)))]
+       (for [key-text key-texts]
+         {:post (:key key-text)
+          :relation (map #(assoc {} :post (:key %) :score (:score %))
+                         (simi/calc-similarity (:text key-text) (remove #(= (:key key-text) (:key %)) key-texts)))})))))
+
+(defn report-post-similarity
+  ([dest post-dir post-dest]
+   (let [similarities (calc-post-similarity post-dir post-dest)]
+     (spit dest (pr-str similarities))))
+  ([]
+   (let [similarities (calc-post-similarity)
+         report-dir (io/file (-> (config/read-config) :report-dir))]
+     (fs/mkdirs report-dir)
+     (spit (io/file report-dir "similarity.edn") (pr-str similarities)))))
--- test/tamaki/post/post_test.clj
+++ test/tamaki/post/post_test.clj
@@ -1,10 +1,29 @@
 (ns tamaki.post.post-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
-            [me.raynes.fs :as rfs]
+            [me.raynes.fs :as fs]
+            [tamaki.post.post :as tpost]
             [tamaki.post.post :as post]))
 
 (deftest loading-posts
   (testing "find the post files "
-    (let [filenames (map #(rfs/base-name %)  (post/post-seq (io/file "dev-resources/tamaki/post")))]
+    (let [filenames (map #(fs/base-name %)  (post/post-seq (io/file "dev-resources/tamaki/post/recursive")))]
       (is (= #{"post.md" "childpost.md"} (set filenames))))))
+
+(def ^:private post-dir "dev-resources/tamaki/post/similarity")
+(def ^:private post-dest "dev-resources/_site")
+
+(deftest test-calc-post-similarity
+
+  (testing "calculation"
+    (let [result  (tpost/calc-post-similarity post-dir post-dest)]
+      (doseq [entity result]
+        (is (fs/exists? (:post entity))))))
+
+  (testing "write similarity"
+    (let [dir "dev-resources/_report/"
+          written (str dir "similarity.edn")]
+      (fs/mkdirs dir)
+      (tpost/report-post-similarity written post-dir post-dest)
+      (let [from-txt (clojure.edn/read-string (slurp written))]
+        (is (= 3 (count from-txt)))))))
