1deb1dc8d082cd5d91a6802de7282b04ee1a70b2
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-03-22 07:23:53 +0900

    spit robots.txt

--- src/tamaki_core/server.clj
+++ src/tamaki_core/server.clj
@@ -21,9 +21,12 @@
   (cpy/copy)
   (page/compile-mds)
   (spit (io/file (io/file (-> (read-config) :dest)) "sitemap.xml")
-         (sitemap/create-sitemap (:page-dir (read-config)) (:post-dir (read-config)) (:url (read-config)))
+         (sitemap/create-sitemap (:page-dir (read-config)) (:post-dir (read-config)) (:url (read-config))))
+  (let [url (string/replace (:url (read-config)) #"([^/])$" "$1/")]
+    (spit (io/file (io/file (-> (read-config) :dest)) "robots.txt")
+          (str "User-agent: *\nSitemap: " url "sitemap.xml\nDisallow:" )))
   (page/gen-paginate-page (-> (read-config) :post-dir))
-  (page/compile-postmds (-> (read-config) :post-dir))))
+  (page/compile-postmds (-> (read-config) :post-dir)))
 
 (defn init [] (tcompile))
 
