0c0c46684f0fe941208289e510351252c5cd3f42
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-04-07 08:24:57 +0900

    add a function to compile sass and css.

--- src/tamaki/css/css.clj
+++ src/tamaki/css/css.clj
@@ -1,10 +1,27 @@
 (ns tamaki.css.css
   (:require [me.raynes.fs :as fs]
+            [tamaki.css.sass :as sass]
+            [clojure.string :as string]
             [clojure.java.io :as io]))
 
 (defn- resolve-sty [src dest]
   (let [sheets (filter #(fs/file? %) (file-seq (io/file src)))]
     (for [sheet sheets]
-      (str dest "/" sheet))))
+      {:src (str sheet) :dest (string/replace (str dest "/" sheet) #"\.(.+)$" ".css")})))
 
 
+(defn compile-styles [styles dest]
+  (letfn [(filter-styles [ptn stys] (filter #(re-seq ptn (:src %)) stys))
+          (compile-css [src-dest]
+            (fs/copy+ (fs/file (:src src-dest)) (fs/file (:dest src-dest))))
+          (compile-sass [src-dest]
+            (if (sass/sass-available?)
+              (do (-> (:dest src-dest) fs/file fs/parent  fs/mkdirs)
+                  (sass/compile-sass (:src src-dest) (:dest src-dest)))))
+          (cmpl [f l] (doseq [e l] (f e)))
+          ]
+    (let [src-dest (reduce #(into (resolve-sty %2 dest) %1) '() styles)
+          csses (filter-styles  #"\.css$" src-dest)
+          sasses (filter-styles  #"\.sass$" src-dest)]
+      (cmpl compile-sass sasses)
+      (cmpl compile-css csses))))
--- test/tamaki/css/css_test.clj
+++ test/tamaki/css/css_test.clj
@@ -7,7 +7,17 @@
 
 (deftest test-css
   (testing "Compiles a sass file."
-    (is (= '("dest/dev-resources/tamaki/css/css/dir/css.css")
+    (is (= '({:src "dev-resources/tamaki/css/css/dir/sass.sass"
+              :dest "dest/dev-resources/tamaki/css/css/dir/sass.css"})
            (#'css/resolve-sty "dev-resources/tamaki/css/css/dir" "dest"))
-    (is (= '("dest/dev-resources/tamaki/css/css/css.css")
-           (#'css/resolve-sty "dev-resources/tamaki/css/css/css.css" "dest"))))))
+        (is (= '({:src "dev-resources/tamaki/css/css/css.css"
+                  :dest "dest/dev-resources/tamaki/css/css/css.css"})
+               (#'css/resolve-sty "dev-resources/tamaki/css/css/css.css" "dest")))))
+
+  (testing "Compiles stylesheets including sass and css."
+    (let [dest (str (fs/temp-dir ""))]
+      (#'css/compile-styles '("dev-resources/tamaki/css/css/dir"
+                               "dev-resources/tamaki/css/css/css.css") dest)
+      (is (= 2
+             (count (map #(.getPath %)
+                         (filter #(and (fs/file? %) (re-seq  #"\.css$" (str %)))  (file-seq (io/file dest))))))))))
