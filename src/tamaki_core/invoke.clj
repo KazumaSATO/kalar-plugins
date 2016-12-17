5f433c26fa98d114f3d8723fc8ca8240e214cc54
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2016-12-17 20:43:30 +0900

    fix invoke

--- src/tamaki_core/invoke.clj
+++ src/tamaki_core/invoke.clj
@@ -1,13 +1,23 @@
 (ns tamaki-core.invoke
   (:require [tamaki-core.config :as config])
   )
-
 (defn invoke
   ([phase config]
-    (doseq [func (get (:hooks config) phase)]
-      (println func)
-      (require (namespace func))
+    (doseq [func (get (:hooks config) (keyword phase))]
+      (-> func namespace symbol require)
       ((-> func resolve var-get) config)))
   ([phase]
     (let [config (config/load-config)]
       (invoke phase config))))
+
+(comment
+(def ^:private clean-lifecycle '(clean))
+
+(defn clean
+  ([config]
+   (doseq [phase clean-lifecycle]
+     (invoke phase config)))
+  ([] (clean (config/load-config))))
+
+
+(def ^:private deploy-lifecycle '()))
