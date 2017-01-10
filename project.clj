581b27d22cc3cb3ead68d39c1eb186021f0e0bcc
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-11 07:54:56 +0900

    change version to 0.3.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tamaki "0.3.0.0-SNAPSHOT"
+(defproject tamaki "0.3.0.0"
   :description "Tamaki, a static site generator"
   :url "https://github.com/satokazuma/tamaki"
   :license {:name "The MIT License"
